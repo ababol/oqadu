0c1581e0cdb886b5122adfb2fab82ea967f1f0bc
Cyril Codron <cyril.codron@outlook.fr>
2014-11-17 14:12:00 +0100

    recommEndations added

--- app/models/Recommendation.js
+++ app/models/Recommendation.js
@@ -3,5 +3,5 @@ module.exports = function(mongoose, Schema) {
   	answerId: {type: Schema.Types.ObjectId, ref: 'Answer', required:true},
   	products: {type: [Schema.Types.ObjectId], ref: 'Product', required:true}
   });
-  return mongoose.model('Recommandation', Recommendation);
+  return mongoose.model('Recommendation', Recommendation);
 };
