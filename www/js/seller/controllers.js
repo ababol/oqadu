f181af5dcbc7b2a6f84d9f9790e7711e3f06cd76
Eric Allard <eric.allard0104@gmail.com>
2014-11-18 14:45:12 +0100

    new charts in statistics part [seller app]

--- www/js/seller/controllers.js
+++ www/js/seller/controllers.js
@@ -71,8 +71,8 @@ angular.module('starter.controllers', [])
   Waitlist.getSize().success(function(data){
     $scope.waitingNumber  = data.size;
   });
-  $scope.peopleToday  = 18;
-  $scope.chartConfig = {
+  $scope.sellStat  = "82%";
+  $scope.chartConfig1 = {
       options: {
         chart: {
           type: 'areaspline',
@@ -81,15 +81,7 @@ angular.module('starter.controllers', [])
         }
       },
       series: [{
-          data: [10, 15, 12, 8, 7, 1, 1, 19, 15, 10]
-      },
-      {
-        data: [5,2,2,3,5,9,7,14,11],
-        type: "column"
-      },
-      {
-          data: [7, 1, 1, 10, 19, 15, 12, 8, 15, 14],
-          type: "line"
+          data: [[8, 10], [9, 15], [10, 12], [11, 8], [12, 7], [13, 1], [14, 1], [15, 19], [16, 15], [17, 10]]
       }],
       
       title: {
@@ -110,6 +102,85 @@ angular.module('starter.controllers', [])
 
       loading: false
   };
+  $scope.chartConfig2 = {
+        chart: {
+            type: 'spline',
+            zoomType: 'y',
+            height: 200
+        },
+        title: {
+            text: '',
+            style: {
+                display: 'none'
+            }
+        },
+        subtitle: {
+            text: '',
+            style: {
+                display: 'none'
+            }
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: { 
+                month: '%e. %b',
+                year: '%b'
+            },
+            title: {
+                text: 'Date'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Clients number'
+            },
+            min: 0
+        },
+        series: [{
+            name: 'This year',
+            data: [
+                [Date.UTC(1970,  9, 18), 0   ],
+                [Date.UTC(1970,  9, 26), 02 ],
+                [Date.UTC(1970, 11,  1), 4],
+                [Date.UTC(1970, 11, 11), 5],
+                [Date.UTC(1970, 11, 25), 8],
+                [Date.UTC(1971,  0,  8), 10],
+                [Date.UTC(1971,  0, 15), 12],
+                [Date.UTC(1971,  1,  1), 13],
+                [Date.UTC(1971,  1,  8), 14],
+                [Date.UTC(1971,  1, 21), 15 ],
+                [Date.UTC(1971,  2, 12), 18],
+                [Date.UTC(1971,  2, 25), 20 ],
+                [Date.UTC(1971,  3,  4), 19]
+            ]
+        }, {
+            name: 'Last Year',
+            data: [
+                [Date.UTC(1970,  9,  9), 0   ],
+                [Date.UTC(1970,  9, 14), 01],
+                [Date.UTC(1970, 10, 28), 03],
+                [Date.UTC(1970, 11, 12), 04],
+                [Date.UTC(1971,  0,  1), 05],
+                [Date.UTC(1971,  0, 24), 05],
+                [Date.UTC(1971,  1,  1), 06],
+                [Date.UTC(1971,  1,  7), 06],
+                [Date.UTC(1971,  1, 23), 07],
+                [Date.UTC(1971,  2,  8), 07],
+                [Date.UTC(1971,  2, 14), 07],
+                [Date.UTC(1971,  2, 24), 08],
+                [Date.UTC(1971,  3,  4), 08 ],
+                [Date.UTC(1971,  3, 18), 09],
+                [Date.UTC(1971,  3, 24), 09 ],
+                [Date.UTC(1971,  4, 16), 03],
+                [Date.UTC(1971,  4, 21), 0]
+            ]
+        }],
+      legend:{
+        enabled: false
+      },
+
+      loading: false
+    };
 })
 
 .controller('AllProductsCtrl', function($scope, Products) {
