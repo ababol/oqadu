8d005fcf67463273c695ad67d69b715dbf47cc38
Arnaud Babol <arnaud.babol@gmail.com>
2014-12-19 17:44:52 +0100

    Node app port 3001 -> 3002

--- app.js
+++ app.js
@@ -95,6 +95,6 @@ app.post('/queue/addUser', function(req, res){
 });
 
 
-http.createServer(app).listen(3001, function() {
-  console.log("Express server listening on port 3001");
+http.createServer(app).listen(3002, function() {
+  console.log("Express server listening on port 3002");
 });
