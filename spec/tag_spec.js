025a3f253a3e5b5de1acb44f70b83f7e942d0951
Eric Allard <eric.allard0104@gmail.com>
2015-01-06 16:20:07 +0100

    add first tag test API V2

---
+++ spec/tag_spec.js
@@ -0,0 +1,15 @@
+var url = "https://oqadu.herokuapp.com",
+  frisby = require('frisby');
+
+
+// test the integrity of the returned JSON by getting the tags list
+frisby.create('Check Products endpoints')
+.get(url + '/api/v2/tags')
+.expectHeaderContains('Content-Type', 'json')
+.expectStatus(200)
+.expectJSONTypes('*', {
+  label: String
+})
+.toss();
+
+
