43db242019980d3cb3f0281e6a5e79b76b5fd8a3
cyrilco <cyril.codron@outlook.fr>
2015-01-06 18:29:48 +0100

    a fail test wrote to watch if travis returns 1 passed test and 1 failed test

--- spec/functionalTests/sellerGUI.js
+++ spec/functionalTests/sellerGUI.js
@@ -1,6 +1,7 @@
 casper.test.begin('assert that an input exists in the products tab', 1, function(test) {
   casper.start('http://localhost:3000/seller.html#/products').then(function() {
     test.assertExists('input');
+    test.assertExists('h1.fakeid');
   }).run(function() {
     test.done();
   });
