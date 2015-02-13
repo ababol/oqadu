9b2790b8ef5c64f6d4e2bc6e7780a47d8ebe33a5
Khezen <simonneaug@gmail.com>
2015-02-13 18:42:25 +0100

    Sauvegarde des données exctraites FINIE yaouhh!!

--- LmDataVacuum/saveExctractedDatas.js
+++ LmDataVacuum/saveExctractedDatas.js
@@ -1,31 +1,56 @@
-var mongoose = require('node-restful').mongoose
-fs = require('fs')
+var mongoose = require('node-restful').mongoose;
+var fs = require('fs');
+var Lock = require('rwlock');
 
 fs.readFile("./exctractedDatas.json", "utf-8", function(err, data){
+
   mongoose.connect('mongodb://public:public@ds045027.mongolab.com:45027/oqadu2');
 
-var routes = require('../app/routes/index');
-routes.forEach(function(route) {
-  route.define();
-});
+  var routes = require('../app/routes/index');
+  routes.forEach(function(route) {
+    route.define();
+  });
 
-var Question = mongoose.model('question')
-var Product = mongoose.model('product')
+  var Question = mongoose.model('question')
+  var Product = mongoose.model('product')
 
   var json = JSON.parse(data);
+  var lock1 = new Lock();
+  var lock2 = new Lock();
 
-  console.log(json.questions.length);
-  console.log(json.products.length);
-  json.questions.forEach(function(question){
-    //questionDocument = new Question(question);
-    //questionDocument.save()
+  lock1.writeLock(function(release1){
+    json.questions.forEach(function(question){
+      lock2.writeLock(function(release2){
+        var questionDocument = new Question(question);
+        questionDocument.save(function(){
+          release2();
+        });
+      });
+    });
+    lock2.writeLock(function(release2){
+      release1();
+      release2();
+    })
   });
 
-  json.products.forEach(function(product){
-    //productDocument = new Product(product);
-    //productDocument.save()
+
+  lock1.writeLock(function(release1){
+    json.products.forEach(function(product){
+      lock2.writeLock(function(release2){
+        var productDocument = new Product(product);
+        productDocument.save(function(){
+          release2();
+        });
+      });
+    });
+    lock2.writeLock(function(release2){
+      release1();
+      release2();
+    })
   });
 
-  mongoose.connection.close()
-
+  lock1.writeLock(function(release1){
+    mongoose.connection.close();
+    release1();
+  });
 });
